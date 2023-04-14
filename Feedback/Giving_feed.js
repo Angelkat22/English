541e3383c60ee7595985c2ba619a12a191fca373
Angelkat2022 <katrinaprivat@gmail.com>
2023-04-14 18:08:52 +0300

    update

---
+++ Feedback/Giving_feed.js
@@ -0,0 +1,16 @@
+
+// English
+// rec5bSOeZFG7QIaJR
+// Описание задания.
+// Choose the topic that you find the most interesting (only ONE) and write your feedback: 
+
+// 2) You are a team lead giving feedback to your team member who is not taking initiative in their work. 
+
+// Hallo, John! 
+// How are you today? 
+// Do you have time to discuss your work on our last project? 
+// I really appreciate your effort on this project. You did a great job. But I noticed, that you didn’t paticipate in our team meetings, didn’t take initiative in the work. I am sure, that you could offer more possibilities to solve some problems or to optimize existing solutions. I think it would be helpful if you could be more active during our meetings and share your ideas. In the future, it would be great if you could  take more initiative not only within our team, but also the all project as a well. I am sure, by maintaining this approach, we will be able to move forward in the meeting project goals together much faster and with better results . 
+// What do think about it? 
+// Have you some ideas to discuss?
+
+// How do you feel about receiving more feedback in the future?
